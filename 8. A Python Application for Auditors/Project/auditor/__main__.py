3c2e007a56a4ce1419378993f273c4ad3db33f4c
Avery Jan <104320849+AveryJan@users.noreply.github.com>
2024-03-26 19:40:03 -0500

    Update __main__.py

--- 8. A Python Application for Auditors/Project/auditor/__main__.py
+++ 8. A Python Application for Auditors/Project/auditor/__main__.py
@@ -5,7 +5,7 @@ This file calls the execute() function in module app.py, passing it the contents
 (Note: so only the command line arguments AFTER 'python auditor' when running this application in codio shell).
 
 Author: Avery Jan
-Date:   6-27-2022
+Date:   6-27-2023
 """
 import sys, app
 app.execute(sys.argv[1:])
