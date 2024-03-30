8e4d86135e8f6e11ec84c2367da04cdb1634b760
Avery Jan <104320849+AveryJan@users.noreply.github.com>
2024-03-29 21:16:02 -0500

    Update app.py

--- 8. A Python Application for Auditors/Project/auditor/app.py
+++ 8. A Python Application for Auditors/Project/auditor/app.py
@@ -2,43 +2,33 @@
 Module that validates the flight school's records.
 
 This is the primary module that does all of the work. It loads the files, loops through
-the lessons, and searches for any takeoffs that violate insurance requirements.
-
-Technically, we could have put many of these functions in __main__.py.  That is the
-main module of this application anyway.  However, for testing purposes we want all
-functions in modules and we only want script code in the file __main__.py
+the lessons, and searches for any takeoffs that violate insurance requirements. For 
+testing purposes all functions are defined in modules and only the script code is 
+in the file __main__.py.
 
 Author: Avery Jan
-Date: 6-27-2022
+Date: 6-27-2023
 """
+
 import utils
 import tests
 import os.path
 import violations
-
-# Uncomment for Stage 2 and Stage 3 Audit
-#import endorsements
-#import inspections
 
 
 def discover_violations(directory,output):
     """
     Searches the dataset directory for any flight lessons that violate regulations.
     
-    This function will call list_weather_violations() to get the list of weather violations.
-    If list_endorsment_violations (optional) is completed, it will call that too, as
-    well as list_inspection_violations.  It will concatenate all of these 2d lists
-    into a single 2d list of violations (so a flight may be listed more than once for
-    each of the three types of violations).
-    
-    If the parameter output is not None, it will create the CSV file with name output
-    and write the 2d list of violations to this file.  This CSV file should have the
-    following header:
+    This function will call list_weather_violations() to get the list of weather  
+    violations. If the parameter output is not None, it will create the CSV file 
+    with name output and write the 2-dimensional list of violations to this file. 
+    This CSV file should have the following header:
     
         STUDENT,AIRPLANE,INSTRUCTOR,TAKEOFF,LANDING,FILED,AREA,REASON
     
-    Regardless of whether output is None, this function will print out the number of
-    violations, as follows:
+    Regardless of whether output is None, this function will print out the number 
+    of violations, as follows:
     
         '23 violations found.'
     
@@ -47,23 +37,22 @@ def discover_violations(directory,output):
         'No violations found.'
     
     Parameter directory: The directory of files to audit
-    Precondition: directory is the name of a directory containing the files 'daycycle.json',
-    'weather.json', 'minimums.csv', 'students.csv', 'teachers.csv', 'lessons.csv',
-    'fleet.csv', and 'repairs.csv'.
+    Precondition: directory is the name of a directory containing the files 
+    'daycycle.json', 'weather.json', 'minimums.csv', 'students.csv', and 
+    'lessons.csv'.
     
     Parameter output: The CSV file to store the results
     Precondition: output is None or a string that is a valid file name
     """
                       
-
-    # get the list of weather violations    
+    # Get the list of weather violations.    
     data = violations.list_weather_violations(directory)
 
-    # add header to the 2-D list
+    # Add a header to the 2-D list.
     header = ['STUDENT','AIRPLANE','INSTRUCTOR','TAKEOFF','LANDING','FILED','AREA','REASON']
     data.insert(0,header)
         
-    # If no violations found, that is, data has the header only. 
+    # If no violations are found, that is, data has the header only. 
     if len(data) == 1:              
         print('No violations found.')
 
@@ -104,11 +93,11 @@ def execute(args):
     This function does not do much error checking beyond counting the number of arguments.
     
     Parameter args: The command line arguments for the application (minus the application name)
-    Precondition: args is a list of strings
+    Precondition: args is a list of strings.
     """
                        
     
-    # Quit if wrong number of arguments
+    # Quit if user call the script with wrong number of arguments
     if len(args) > 2 or len(args) < 1:
         print('Usage: python auditor dataset [output.csv]')
 
